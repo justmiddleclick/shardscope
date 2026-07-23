98d725278994a2e569bde327bc0cec92ebe41917
justmiddleclick <justmiddleclick@gmail.com>
2026-07-22 23:47:23 -0400

    Add hunting and fusion database

---
+++ shardData.js
@@ -0,0 +1,29 @@
+const shardData = [
+  /*
+  Each shard will eventually look like this:
+
+  {
+    name: "Shard Name",
+    bazaarId: "SHARD_PRODUCT_ID",
+
+    hunting: {
+      huntable: true,
+      location: "Location",
+      method: "How it is hunted",
+      tool: "Required tool",
+      difficulty: "Easy",
+      estimatedPerHour: 0
+    },
+
+    fusion: {
+      canBeCreatedByFusion: true,
+      ingredients: [
+        {
+          bazaarId: "SHARD_INGREDIENT",
+          amount: 1
+        }
+      ]
+    }
+  }
+  */
+];
